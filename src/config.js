2ecd349ed3c9687b44dfd53c1957140e53ea3f97
Enrique Montemayor <eemontemayor@live.com>
2019-10-10 16:51:00 -0500

    x

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: 'https://immense-fortress-31634.herokuapp.com/'
+  API_ENDPOINT: 'https://immense-fortress-31634.herokuapp.com'
     // API_ENDPOINT:'http://localhost:8000',
 }
